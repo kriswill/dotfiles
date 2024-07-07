7a916546d2ca35ff80241ffd6907525fd9b30a57
Kris Williams <115474+kriswill@users.noreply.github.com>
2024-07-06 22:58:04 -0700

    simplify flake

--- flake.nix
+++ flake.nix
@@ -20,19 +20,16 @@
 
   outputs =
     inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } (
-      { ... }:
-      {
-        systems = [
-          "x86_64-linux"
-          "aarch64-linux"
-          "aarch64-darwin"
-        ];
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
 
-        imports = [
-          ./machines
-          ./per-system
-        ];
-      }
-    );
+      imports = [
+        ./machines
+        ./per-system
+      ];
+    };
 }
