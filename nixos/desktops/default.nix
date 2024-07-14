b1a0b8b776c675cc2f1c049aa5c345f629557f8a
Kris Williams <115474+kriswill@users.noreply.github.com>
2024-07-14 13:33:20 -0700

    fix sddm monitor configuration

--- nixos/desktops/default.nix
+++ nixos/desktops/default.nix
@@ -6,6 +6,7 @@
   lib,
   ...
 }:
+
 {
   options.gnome = {
     enable = lib.mkEnableOption "Gnome";
@@ -18,48 +19,8 @@
   imports = [
     ./gnome
     ./hyprland
+    ./sddm.nix
   ];
 
-  config = {
-    services.displayManager = {
-      defaultSession = "gnome";
-      sddm = {
-        enable = true;
-        wayland.enable = true;
-        # theme = "where_is_my_sddm_theme";
-        theme = "eucalyptus-drop";
-        package = lib.mkDefault pkgs.kdePackages.sddm;
-      };
-    };
-
-    environment.systemPackages =
-      with packages;
-      [ sddm-eucalyptus-drop ]
-      ++ (with pkgs.libsForQt5.qt5; [
-        qtgraphicaleffects
-        qtsvg
-      ])
-      ++ (with pkgs; [
-        qt6.qt5compat
-        #
-        # (where-is-my-sddm-theme.override {
-        #   variants = [ "qt6" ];
-        #   themeConfig.General = {
-        #     background = pkgs.wallpapers.yoda-dhagoba;
-        #     backgroundMode = "fill";
-        #     passwordInputRadius = 10;
-        #     blurRadius = 0;
-        #     usersFontSize = 16;
-        #     basicTextColor = "#ffffff";
-        #     passwordInputBackground = "#60ffffff";
-        #     passwordInputWidth = 0.2;
-        #     passwordFontSize = 48;
-        #     sessionsFontSize = 36;
-        #     showUsersByDefault = false;
-        #     showSessionsByDefault = true;
-        #   };
-        # })
-      ]);
-
-  };
+  config.services.displayManager.defaultSession = lib.mkDefault "gnome";
 }
---
+++ nixos/desktops/sddm.nix
@@ -0,0 +1,65 @@
+{
+  inputs,
+  pkgs,
+  packages,
+  config,
+  lib,
+  ...
+}:
+
+let
+  xcfg = config.services.xserver;
+
+  weston-ini = pkgs.writeText "weston.ini" ''
+    [core]
+    xwayland=false
+    shell=fullscreen-shell.so
+
+    [keyboard]
+    keymap_model=${builtins.toString xcfg.xkb.model}
+    keymap_layout=${builtins.toString xcfg.xkb.layout}
+    keymap_options=${builtins.toString xcfg.xkb.options}
+    keymapvariant=
+
+    [libinput]
+    enable-tap=true
+    left-handed=false
+
+    [output]
+    name=DP-2
+    mode=3440x1440@240
+
+    [output]
+    name=DP-1
+    mode=off
+  '';
+
+  weston-command = lib.concatStringsSep " " [
+    "${lib.getExe pkgs.weston}"
+    "--shell=kiosk"
+    "-c ${weston-ini}"
+  ];
+in
+{
+  services.displayManager = {
+    sddm = {
+      enable = true;
+      wayland = {
+        enable = true;
+        compositor = lib.mkForce "weston";
+        compositorCommand = weston-command;
+      };
+      # custom themes need this package to work with qt5 sddm greeter
+      package = pkgs.kdePackages.sddm;
+      theme = "eucalyptus-drop";
+    };
+  };
+
+  environment.systemPackages =
+    [ packages.sddm-eucalyptus-drop ]
+    ++ [ pkgs.qt6.qt5compat ]
+    ++ [
+      pkgs.libsForQt5.qt5.qtgraphicaleffects
+      pkgs.libsForQt5.qt5.qtsvg
+    ];
+}
