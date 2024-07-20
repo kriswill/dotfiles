c2e6e85807c42ed397a98951af04742ce6741787
Kris Williams <115474+kriswill@users.noreply.github.com>
2024-07-20 00:11:23 -0700

    kitty conf improvements

--- home/k/programs/kitty.nix
+++ home/k/programs/kitty.nix
@@ -5,13 +5,17 @@ _:
     enable = true;
     font = {
       name = "SauceCodePro Nerd Font";
-      size = 14;
+      size = 14.0;
     };
-    shellIntegration.enableZshIntegration = true;
 
     extraConfig = builtins.readFile ./kitty-theme/adwaita.conf;
 
     settings = {
+      update_check_interval = 0;
+      # fade | slant | separator | powerline | custom | hidden
+      tab_bar_style = "powerline";
+      # angled | slanted | round
+      tab_powerline_style = "slanted";
       background_opacity = "0.9";
       confirm_os_window_close = 0;
       copy_on_select = "clipboard";
@@ -21,8 +25,12 @@ _:
       placement_strategy = "center";
       scrollback_lines = 20000;
       # term = "xterm-256color";
-      visual_bell_duration = "0.1";
+      # visual_bell_duration = "0.1";
       # XDG_CURRENT_DESKTOP = "GNOME";
+    };
+    shellIntegration = {
+        mode = "enabled";
+        enableZshIntegration = true;
     };
   };
 }
