06e624628a4eeae8ded797f7a50ca61f1735f3e4
Kris Williams <115474+kriswill@users.noreply.github.com>
2024-07-28 22:17:03 -0700

    enable polkit and gnome auth agent in Hyprland

--- nixos/desktops/hyprland/default.nix
+++ nixos/desktops/hyprland/default.nix
@@ -79,26 +79,26 @@
       configPackages = with pkgs; [ xdg-desktop-portal-gtk ];
     };
 
-    # security = {
-    #   polkit.enable = true;
-    #   #pam.services.ags = {};
-    # };
+    security = {
+      polkit.enable = true;
+      #pam.services.ags = {};
+    };
 
-    # systemd = {
-    #   user.services.polkit-gnome-authentication-agent-1 = {
-    #     description = "polkit-gnome-authentication-agent-1";
-    #     wantedBy = ["graphical-session.target"];
-    #     wants = ["graphical-session.target"];
-    #     after = ["graphical-session.target"];
-    #     serviceConfig = {
-    #       Type = "simple";
-    #       ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-    #       Restart = "on-failure";
-    #       RestartSec = 1;
-    #       TimeoutStopSec = 10;
-    #     };
-    #   };
-    # };
+    systemd = {
+      user.services.polkit-gnome-authentication-agent-1 = {
+        description = "polkit-gnome-authentication-agent-1";
+        wantedBy = ["graphical-session.target"];
+        wants = ["graphical-session.target"];
+        after = ["graphical-session.target"];
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+          Restart = "on-failure";
+          RestartSec = 1;
+          TimeoutStopSec = 10;
+        };
+      };
+    };
 
     # services = {
     #   gvfs.enable = true;
