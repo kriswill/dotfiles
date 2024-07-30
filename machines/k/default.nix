362961634269e3e6d34cf9ab9520ede50a8e8e41
Kris Williams <115474+kriswill@users.noreply.github.com>
2024-07-30 14:49:23 -0700

    add iproute2mac for darwin

--- machines/k/default.nix
+++ machines/k/default.nix
@@ -5,6 +5,7 @@
   
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
+    unstable.darwin.iproute2mac
     home-manager
   ];
   security.pam.enableSudoTouchIdAuth = true;
