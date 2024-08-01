120c44839cee4f497668e442b1863c68bc5f7ae1
Kris Williams <115474+kriswill@users.noreply.github.com>
2024-08-01 14:57:28 -0700

    remove ruler from nvimtree

--- home/programs/neovim/nvim/lua/plugins/nvim-tree.lua
+++ home/programs/neovim/nvim/lua/plugins/nvim-tree.lua
@@ -11,7 +11,8 @@ return {
     nvimtree.setup({
       view = {
         width = 35,
-        relativenumber = true,
+        number = false,
+        relativenumber = false,
       },
       -- change folder arrow icons
       renderer = {
