f0d2ff49110ba585c29e3c27101bd0fe067f949b
Kris Williams <115474+kriswill@users.noreply.github.com>
2024-08-02 21:32:01 -0700

    small changes to polybar

--- home/programs/i3/polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload toph &
-  done
-else
-  polybar --reload toph &
-fi
