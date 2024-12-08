887d44be3d6ffc76bb15d3d42dd442c4a0f45a61
Kris Williams <115474+kriswill@users.noreply.github.com>
2024-12-08 17:32:31 -0600

    add git config

--- home/k.nix
+++ home/k.nix
@@ -1,1 +1,9 @@
-{ }
+{ pkgs, ... }:
+{
+  imports = [ ./git.nix ];
+  # home.packages = with pkgs; [
+  #   gnome-terminal
+  # ];
+  # programs.kitty.enable = false;
+  # custom.terminal = "gnome-terminal";
+}
