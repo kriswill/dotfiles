c0faffa5bb89baf8d4a64b911e10d4090400f614
Kris Williams <115474+kriswill@users.noreply.github.com>
2024-12-08 22:18:28 -0800

    wayland clipboard, 12 hour clock

--- hosts/yoda/users/k/hyprland/default.nix
+++ hosts/yoda/users/k/hyprland/default.nix
@@ -1,5 +1,13 @@
-{ ... }:
+{ pkgs, lib, ... }:
 
-{
+with pkgs; {
   imports = [ ./settings.nix ];
+
+  home.packages = [
+    wl-clipboard
+  ];
+
+  programs.waybar.bottomBar.settings = {
+    clock.format = lib.mkForce " {:%I:%M %p  %m.%d.%Y}";
+  };
 }
