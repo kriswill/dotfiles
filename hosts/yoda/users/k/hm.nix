fd63f84f4101fc07c67d2fcab45d0b627982f093
Kris Williams <115474+kriswill@users.noreply.github.com>
2024-12-09 21:42:55 -0800

    fix hyprpaper

--- hosts/yoda/users/k/hm.nix
+++ hosts/yoda/users/k/hm.nix
@@ -1,26 +1,9 @@
-{ wallpapers, icons, ... }:
+{ icons, ... }:
 {
   imports = [ ./hyprland ];
   # wayland.windowManager.hyprland.systemd.enable = false;
   programs.discord.enable = true;
   # todo Remove this turd
   stylix.enable = true;
-  services.hyprpaper =
-    let
-      dp1 = builtins.fetchurl wallpapers.yoda-dagoba-1;
-      dp2 = builtins.fetchurl wallpapers.yoda-dagoba-2;
-    in
-    {
-      enable = true;
-      settings = {
-        ipc = "on";
-        splash = false;
-        preload = [ dp1 dp2 ];
-        wallpaper = [
-          "DP-1, ${dp1}"
-          "DP-2, ${dp2}"
-        ];
-      };
-    };
   programs.fastfetch.image = builtins.fetchurl icons.yoda;
 }
