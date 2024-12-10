792a5b1b2fc36dec9758df72102eb4eceeb1b79a
Kris Williams <115474+kriswill@users.noreply.github.com>
2024-12-09 22:36:50 -0800

    kitty config

--- home/k.nix
+++ home/k.nix
@@ -1,9 +1,10 @@
-{ inputs, lib, hostName, ... }:
+{ inputs, pkgs, lib, hostName, ... }:
 {
   imports = [
     ./git.nix
     inputs.nix-config.homeProfiles.essentials
     ./zsh
+    ./kitty
   ] ++ lib.optionals (builtins.pathExists ../hosts/${hostName}/users/k) [
     ../hosts/${hostName}/users/k/hm.nix
   ];
@@ -15,4 +16,8 @@
   custom.browser = "brave";
   # programs.firefox.enable = true;
   programs.yazi.enable = true;
+  stylix.targets.kitty.enable = false;
+  home.packages = with pkgs.nerd-fonts; [
+    sauce-code-pro
+  ];
 }
---
+++ home/kitty/default.nix
@@ -0,0 +1,35 @@
+{ lib, ... }:
+
+{
+  programs.kitty = {
+    font = {
+      name = "SauceCodePro Nerd Font";
+      size = 14.0;
+    };
+
+    extraConfig = builtins.readFile ./adwaita.conf;
+
+    settings = lib.mkForce {
+      update_check_interval = 0;
+      # fade | slant | separator | powerline | custom | hidden
+      tab_bar_style = "powerline";
+      # angled | slanted | round
+      tab_powerline_style = "slanted";
+      background_opacity = "0.9";
+      confirm_os_window_close = 0;
+      copy_on_select = "clipboard";
+      enable_audio_bell = "no";
+      hide_window_decorations = "yes";
+      # linux_display_server = "wayland";
+      placement_strategy = "bottom";
+      scrollback_lines = 40000;
+      # term = "xterm-256color";
+      # visual_bell_duration = "0.1";
+      # XDG_CURRENT_DESKTOP = "GNOME";
+    };
+    shellIntegration = {
+      mode = "enabled";
+      enableZshIntegration = true;
+    };
+  };
+}
