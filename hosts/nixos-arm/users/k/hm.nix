4ca57270eaf7762c3888683a311be7ffbd6554a1
Kris Williams <115474+kriswill@users.noreply.github.com>
2024-12-15 18:09:26 -0800

    add blue-marble-2 wallpaper - fix rofi keybind

--- hosts/nixos-arm/users/k/hm.nix
+++ hosts/nixos-arm/users/k/hm.nix
@@ -1,13 +1,19 @@
-{ icons, lib, pkgs, ... }:
+{ wallpapers, icons, lib, pkgs, ... }:
 {
   imports = [ ./hyprland ];
   # wayland.windowManager.hyprland.systemd.enable = false;
   programs.discord.enable = true;
   # todo Remove this turd
-  stylix.enable = true;
+  stylix = {
+    enable = true;
+    image = builtins.fetchurl wallpapers.blue-marble-2;
+  };
+
   programs.fastfetch.image = builtins.fetchurl icons.yoda;
-  xsession.windowManager.i3.config.keybindings = {
-    "Mod4+space" = "exec /nix/store/3n37yan2fz8np2as6xszjhyqwqajl4zs-rofi-1.7.5/bin/rofi -show";
-    "Mod4+Mod1+space" = "exec ${lib.getExe pkgs.rofi} -show";
+  xsession.windowManager.i3.config = {
+    keybindings = {
+      "Mod4+space" = "exec /nix/store/3n37yan2fz8np2as6xszjhyqwqajl4zs-rofi-1.7.5/bin/rofi -show";
+      "Mod4+Mod1+space" = "exec ${lib.getExe pkgs.rofi} -show";
+    };
   };
 }
