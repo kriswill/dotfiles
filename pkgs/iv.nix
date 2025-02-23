4de65beb9ed3928bc172a0dc73ca296ff6044d84
Kris Williams <115474+kriswill@users.noreply.github.com>
2025-02-22 18:27:56 -0800

    WIP iv.nix package

---
+++ pkgs/iv.nix
@@ -0,0 +1,46 @@
+{
+  stdenv,
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  pkg-config,
+  resvg,
+  vips,
+  makeWrapper,
+}:
+buildGoModule rec {
+  pname = "iv";
+  version = "0.7.2";
+
+  src = fetchFromGitHub {
+    owner = "kenshaw";
+    repo = "iv";
+    rev = "v${version}";
+    hash = "sha256-cdsyrtXWkOQKeq6FmVCNUdoFABJhy9FUaGVX4Akmjf4=";
+  };
+
+  vendorHash = "sha256-pNhDgv6l2qCmz+e0Kwd/AtYX6qfaZzfbtsKT/dC4300=";
+
+  buildInputs = [ resvg vips ];
+
+  ldflags = [
+    "-s"
+    "-w"
+  ];
+
+  trimpath = true;
+  # env.CGO_LD_FLAGS = "-L ${resvg}/lib -lresvg";
+  nativeBuildInputs = [ pkg-config vips makeWrapper ]; 
+
+  postFixup = ''
+    wrapProgram $out/bin/${pname} \
+      --set LD_LIBRARY_PATH ${resvg}/lib \
+      --set DYLD_LIBRARY_PATH ${resvg}/lib
+  '';
+  meta = {
+    description = "A command-line image viewer using terminal graphics";
+    homepage = "https://github.com/kenshaw/iv";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ kriswill ];
+  };
+}
