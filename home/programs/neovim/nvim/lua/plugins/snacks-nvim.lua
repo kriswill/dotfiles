538db2193ef6f89448443c3cac0cd8c558d0347c
Kris Williams <115474+kriswill@users.noreply.github.com>
2025-03-10 11:10:13 -0700

    nvim.lock

--- home/programs/neovim/nvim/lua/plugins/snacks-nvim.lua
+++ home/programs/neovim/nvim/lua/plugins/snacks-nvim.lua
@@ -102,20 +102,6 @@ return {
     { "<c-_>", function() Snacks.terminal() end, desc = "which_key_ignore", },
     { "]]", function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" }, },
     { "[[", function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" }, },
-    -- { "<leader>N", desc = "Neovim News", function() Snacks.win({
-    --       file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-    --       width = 0.6,
-    --       height = 0.6,
-    --       wo = {
-    --         spell = false,
-    --         wrap = false,
-    --         signcolumn = "yes",
-    --         statuscolumn = " ",
-    --         conceallevel = 3,
-    --       },
-    --     })
-    --   end,
-    -- },
   },
   -- stylua: ignore end
   ---@type snacks.Config
@@ -183,7 +169,7 @@ return {
 ⠈⢠⣾⣿⣿⢿⣿⣿⣿⣿⣿⡟⠉⠀⣠⣶⣶⣿⣿⣄⠀       
 ⢠⡿⣽⡿⢣⣿⣿⣿⣿⣿⣿⠀⠀⢸⠋⠁⠀⡴⠟⠛⠿⣦⣄⠀⡀   
 ⣾⢡⣿⠃⣼⡿⣿⣿⢸⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠐⠚⠛⣿⣿⠇   
-⡇⢸⣿⠀⢿⡇⣿⣿⣇⠹⣿⣿⣦⣤⣴⠆     ⠐⠁⠹
+⡇⢸⣿⠀⢿⡇⣿⣿⣇⠹⣿⣿⣦⣤⣴⠆     ⠐⠁⠹    
 ]],
       },
     },
