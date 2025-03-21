65e561db5f3bac74955683fb1345c85730d7940e
Kris Williams <115474+kriswill@users.noreply.github.com>
2025-03-21 16:06:16 -0700

    add conform.nvim for file formatting

---
+++ home/programs/neovim/nvim/lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "black" },
+      rust = { "rustfmt", lsp_format = "fallback" },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+      typescript = { "prettierd", "prettier", stop_after_first = true },
+    },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    },
+  },
+}
