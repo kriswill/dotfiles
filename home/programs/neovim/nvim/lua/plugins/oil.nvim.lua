b377f3670e98e30c9c37381216d2ea3ae34d19af
Kris Williams <115474+kriswill@users.noreply.github.com>
2025-03-21 16:06:30 -0700

    show hidden files in oil

--- home/programs/neovim/nvim/lua/plugins/oil.nvim.lua
+++ home/programs/neovim/nvim/lua/plugins/oil.nvim.lua
@@ -2,8 +2,12 @@ return {
   "stevearc/oil.nvim",
   --@module 'oil'
   --@type oil.SetupOpts
-  opts = {},
-  dependencies = {{ "echasnovski/mini.icons", opts = {} }},
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+  },
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
   lazy = false,
   keys = {
     { "-", "<cmd>Oil --float<CR>", mode = "n", desc = "Open Parent Dir" },
