911005ff0813f827ab990c0cd07b5d338d5cb9cb
Kris Williams <115474+kriswill@users.noreply.github.com>
2025-03-21 16:06:54 -0700

    add treesitter-context

---
+++ home/programs/neovim/nvim/lua/plugins/treesitter-context.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-treesitter/nvim-treesitter-context",
+  event = "VeryLazy",
+  opts = { mode = "cursor", max_lines = 3 },
+  keys = {
+    {
+      "<leader>ut",
+      function()
+        local tsc = require("treesitter-context")
+        tsc.toggle()
+      end,
+      desc = "Toggle Treesitter Context",
+    },
+  },
+}
