b9032115700eb1b7906f07ff634497fc5a95f706
Kris Williams <115474+kriswill@users.noreply.github.com>
2025-03-22 13:05:17 -0700

    more conform formatter configs

--- home/programs/neovim/nvim/lua/plugins/conform.lua
+++ home/programs/neovim/nvim/lua/plugins/conform.lua
@@ -2,15 +2,23 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
+      html = { "prettierd", "prettier" },
+      javascript = { "prettierd", "prettier" },
       lua = { "stylua" },
+      markdown = { "deno_fmt", "prettierd", "prettierd" },
+      nix = { "nixfmt", "nixpkgs-fmt" },
       python = { "isort", "black" },
-      rust = { "rustfmt", lsp_format = "fallback" },
-      javascript = { "prettierd", "prettier", stop_after_first = true },
-      typescript = { "prettierd", "prettier", stop_after_first = true },
+      rust = { "rustfmt" },
+      typescript = { "prettierd", "prettier" },
+      xml = { "xmllint", "xmlformat" },
+      yaml = { "prettierd", "prettier", "yamlfmt", "yq" },
+    },
+    default_format_opts = {
+      lsp_format = "fallback",
+      stop_after_first = true,
     },
     format_on_save = {
       timeout_ms = 500,
-      lsp_format = "fallback",
     },
   },
 }
