e0ece754b828b7d2ac0290538ffaca6ce660fbf1
Kris Williams <115474+kriswill@users.noreply.github.com>
2025-04-20 16:26:30 -0700

    remove Mason from lsp.lua

--- home/programs/neovim/nvim/lua/plugins/lsp.lua
+++ home/programs/neovim/nvim/lua/plugins/lsp.lua
@@ -2,13 +2,6 @@ return {
   -- Main LSP Configuration
   "neovim/nvim-lspconfig",
   dependencies = {
-    -- Automatically install LSPs and related tools to stdpath for Neovim
-    -- Mason must be loaded before its dependents so we need to set it up here.
-    -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
-    -- { "williamboman/mason.nvim", opts = {} },
-    -- "williamboman/mason-lspconfig.nvim",
-    -- "WhoIsSethDaniel/mason-tool-installer.nvim",
-
     -- Useful status updates for LSP.
     { "j-hui/fidget.nvim", opts = {} },
     { "ibhagwan/fzf-lua", opts = {} },
@@ -99,11 +92,7 @@ return {
         ---@param bufnr? integer some lsp support methods only in specific files
         ---@return boolean
         local function client_supports_method(client, method, bufnr)
-          if vim.fn.has("nvim-0.11") == 1 then
-            return client:supports_method(method, bufnr)
-          else
-            return client.supports_method(method, { bufnr = bufnr })
-          end
+          return client:supports_method(method, bufnr)
         end
 
         -- The following two autocommands are used to highlight references of the
@@ -248,42 +237,5 @@ return {
       opts.capabilities = vim.tbl_deep_extend("force", {}, capabilities, opts.capabilities or {})
       require("lspconfig")[name].setup(opts)
     end
-
-    -- Ensure the servers and tools above are installed
-    --
-    -- To check the current status of installed tools and/or manually install
-    -- other tools, you can run
-    --    :Mason
-    --
-    -- You can press `g?` for help in this menu.
-    --
-    -- `mason` had to be setup earlier: to configure its options see the
-    -- `dependencies` table for `nvim-lspconfig` above.
-    --
-    -- You can add other tools here that you want Mason to install
-    -- for you, so that they are available from within Neovim.
-    --
-    --
-    -- local ensure_installed = vim.tbl_keys(servers or {})
-    -- vim.list_extend(ensure_installed, {
-    --   "stylua", -- Used to format Lua code
-    --   "prettierd", -- Used to format JS/CSS
-    -- })
-    -- require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-    --
-    -- require("mason-lspconfig").setup({
-    --   ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-    --   automatic_installation = false,
-    --   handlers = {
-    --     function(server_name)
-    --       local server = servers[server_name] or {}
-    --       -- This handles overriding only values explicitly passed
-    --       -- by the server configuration above. Useful when disabling
-    --       -- certain features of an LSP (for example, turning off formatting for ts_ls)
-    --       server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-    --       require("lspconfig")[server_name].setup(server)
-    --     end,
-    --   },
-    -- })
   end,
 }
