b4553bc4d8c867ea30b8f1325341cf74d923845c
Kris Williams <115474+kriswill@users.noreply.github.com>
2025-05-02 18:15:19 -0700

    refine lua_ls, small refactor for snacks.nvim config

---
+++ home/programs/neovim/nvim/lua/plugins/snacks/init.lua
@@ -0,0 +1,60 @@
+local dashboard = require("plugins.snacks.dashboard")
+local keys = require("plugins.snacks.keys")
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  keys = keys,
+  init = function()
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "VeryLazy",
+      callback = function()
+        -- Setup some globals for debugging (lazy-loaded)
+        _G.dd = function(...) Snacks.debug.inspect(...) end
+        _G.bt = function() Snacks.debug.backtrace() end
+        vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+        -- Create some toggle mappings
+        Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+        Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+        Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+        Snacks.toggle.diagnostics():map("<leader>ud")
+        Snacks.toggle.line_number():map("<leader>ul")
+        Snacks.toggle
+          .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
+          :map("<leader>uc")
+        Snacks.toggle.treesitter():map("<leader>uT")
+        Snacks.toggle.inlay_hints():map("<leader>uh")
+        Snacks.toggle.indent():map("<leader>ug")
+        Snacks.toggle.dim():map("<leader>uD")
+      end,
+    })
+  end,
+  ---@type snacks.Config
+  opts = {
+    bigfile = { enabled = true },
+    explorer = {
+      enabled = true,
+      layout = {
+        preset = "custom_sidebar",
+        layout = {
+          width = 0.22,
+          position = "right",
+        },
+      },
+    },
+    image = { enabled = true },
+    indent = { enabled = false },
+    input = { enabled = true },
+    notifier = { enabled = true, timeout = 3000 },
+    picker = {
+      enabled = true,
+    },
+    quickfile = { enabled = true },
+    scope = { enabled = false },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = false },
+    words = { enabled = true },
+    dashboard = dashboard,
+  },
+}
