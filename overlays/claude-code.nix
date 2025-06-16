e9c16b658f6e717df2249f4fd2bccad072d97baf
Kris Williams <115474+kriswill@users.noreply.github.com>
2025-06-15 18:09:28 -0700

    update claude v1.0.22 -> v1.0.24

--- overlays/claude-code.nix
+++ overlays/claude-code.nix
@@ -1,9 +1,9 @@
 final: prev: {
   claude-code = prev.claude-code.overrideAttrs (oldAttrs: rec {
-    version = "1.0.22";
+    version = "1.0.24";
     src = prev.fetchzip {
       url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-      hash = "sha256-Gn+AzZysuYsZDMzcXlzDMWSWeJS3L7itvlGJq4kYha0=";
+      hash = "sha256-12nmnVM0/+rhWrkIQXttASKPZgGQMvrzWF/JDwR7If4=";
     };
   });
 }
