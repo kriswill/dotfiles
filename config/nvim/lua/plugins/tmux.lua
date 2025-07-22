3a19ac6dfdabec187f124769962a61fe0f62215b
kriswill <115474+kriswill@users.noreply.github.com>
2025-07-22 16:25:42 -0700

    nvim: keybinds for in-editor terminal to match panel movement

--- config/nvim/lua/plugins/tmux.lua
+++ config/nvim/lua/plugins/tmux.lua
@@ -12,5 +12,11 @@ return {
         next = "<C-Space>",
       },
     })
+    -- Terminal mode window navigation
+    vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]], { desc = "Move to left window from terminal" })
+    vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], { desc = "Move to window below from terminal" })
+    vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], { desc = "Move to window above from terminal" })
+    vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], { desc = "Move to right window from terminal" })
+    vim.keymap.set("t", "<M-Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
   end,
 }
