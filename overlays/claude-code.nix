94bef54736741e70818c5e8bbd73393fe249b8c6
kriswill <115474+kriswill@users.noreply.github.com>
2025-08-06 16:44:12 -0700

    upgrade claude-code 1.0.62 -> 1.0.70

--- overlays/claude-code.nix
+++ overlays/claude-code.nix
@@ -1,9 +1,9 @@
 _final: prev: {
   claude-code = prev.claude-code.overrideAttrs (_oldAttrs: rec {
-    version = "1.0.62";
+    version = "1.0.70";
     src = prev.fetchzip {
       url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-      hash = "sha256-bzbnLVAtMXr4XHmHqSOn3jxB+6BsFCd3gd/j34Zkwi0=";
+      hash = "sha256-7nqhJNhO+QollwVtVlKDYHOlPDT6Erk6wI/voiAYXY4=";
     };
   });
 }
