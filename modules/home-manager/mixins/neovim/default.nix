f91374a80fbe67a5e0600322ce9075dfd2953270
kriswill <115474+kriswill@users.noreply.github.com>
2025-08-11 11:07:05 -0700

    optimize evaluation time for neovim extraPackages
    turns out that using `with pkgs` on nixpkgs is inefficient due to the
    size of the list, costing extra evaluation time.

--- modules/home-manager/mixins/neovim/default.nix
+++ modules/home-manager/mixins/neovim/default.nix
@@ -15,53 +15,65 @@
       vimAlias = true;
       withRuby = false;
 
-      extraPackages = with pkgs; [
-        ## Formatters  ──────────────────────────────────────────────
-        black # Python
-        gofumpt # stricter gofmt
-        isort # Python
-        luajitPackages.jsregexp # luasnip
-        nixfmt-rfc-style
-        prettierd # Multiple language formatter
-        shfmt
-        stylua # Lua
-        yamlfmt
-
-        ## LSP servers  ─────────────────────────────────────────────
-        bash-language-server # Bash
-        buf # bufls
-        dockerfile-language-server-nodejs # Dockerfile
-        docker-compose-language-service # Docker Compose
-        gopls # Go
-        lua-language-server # Lua (lua_ls)
-        lua5_1
-        luarocks
-        marksman # Markdown
-        nil
-        nodePackages.typescript
-        pyright
-        rust-analyzer # Rust
-        vscode-langservers-extracted # json, HTML, CSS, ESLint
-        vtsls # LSP wrapper for typescript extension of vscode
-        yaml-language-server # YAML (yamlls)
-
-        go_1_24
-        (go-tools.override { buildGoModule = buildGo124Module; })
-
-        ## Tools  ───────────────────────────────────────────────────
-        cargo
-        cmake
-        delve # golang debugger `dlv`
-        fswatch # file watcher - replaces libuv.fs_event in neovim v0.10
-        git
-        gnumake
-        imagemagick
-        nodejs
-        sqlite
-        tree-sitter
-        wget
-      ];
+      extraPackages =
+        let
+          ## Formatters  ──────────────────────────────────────────────
+          formatters = builtins.attrValues {
+            inherit (pkgs)
+              black # Python
+              gofumpt # stricter gofmt
+              isort # Python
+              nixfmt-rfc-style
+              prettierd # Multiple language formatter
+              shfmt
+              stylua # Lua
+              yamlfmt
+              ;
+            jsregexp = pkgs.luajitPackages.jsregexp; # luasnip
+          };
+          ## LSP servers  ─────────────────────────────────────────────
+          lsp-servers = builtins.attrValues {
+            inherit (pkgs)
+              bash-language-server # Bash
+              buf # bufls
+              dockerfile-language-server-nodejs # Dockerfile
+              docker-compose-language-service # Docker Compose
+              gopls # Go
+              lua-language-server # Lua (lua_ls)
+              lua5_1
+              luarocks
+              marksman # Markdown
+              nil
+              pyright
+              rust-analyzer # Rust
+              vscode-langservers-extracted # json, HTML, CSS, ESLint
+              vtsls # LSP wrapper for typescript extension of vscode
+              yaml-language-server # YAML (yamlls)
+              go # golang
+              go-tools
+              ;
+            typescript = pkgs.nodePackages.typescript;
+          };
+          ## Tools  ───────────────────────────────────────────────────
+          tools = builtins.attrValues {
+            inherit (pkgs)
+              cargo
+              cmake
+              delve # golang debugger `dlv`
+              fswatch # file watcher - replaces libuv.fs_event in neovim v0.10
+              git
+              gnumake
+              imagemagick
+              nodejs
+              sqlite
+              tree-sitter
+              wget
+              ;
+          };
+        in
+        formatters ++ lsp-servers ++ tools;
     };
+
     xdg.configFile =
       let
         # used to link files from <repo>/config/nvim to ~/.config/nvim
