a8ef7d903978c0d662201d8da1b21300c9685aa5
kriswill <115474+kriswill@users.noreply.github.com>
2025-08-23 11:40:54 -0700

    update claude code 1.0.86 -> 1.0.89

--- overlays/claude-code.nix
+++ overlays/claude-code.nix
@@ -1,9 +1,9 @@
 _final: prev: {
   claude-code = prev.claude-code.overrideAttrs (_oldAttrs: rec {
-    version = "1.0.86";
+    version = "1.0.89";
     src = prev.fetchzip {
       url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-      hash = "sha256-mVXS75KgeKgD7EI5t9X6+TkwjBFyBLOo4/m50sS9XdA=";
+      hash = "sha256-ULOL4emd7VZNvHD5Hk3YI1Cyb1Q1YMCcDDxlRDUM1dc=";
     };
   });
 }
