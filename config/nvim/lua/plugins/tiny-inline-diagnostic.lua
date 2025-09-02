d9f272ed177c37af8c5db06e8d030c2bd5004788
kriswill <115474+kriswill@users.noreply.github.com>
2025-09-02 14:56:54 -0700

    disable tiny-inline-diagnostic

--- config/nvim/lua/plugins/tiny-inline-diagnostic.lua
+++ config/nvim/lua/plugins/tiny-inline-diagnostic.lua
@@ -1,10 +1,10 @@
 return {
-  "rachartier/tiny-inline-diagnostic.nvim",
-  -- VeryLazy, Or LspAttach
-  event = "LspAttach",
-  priority = 1000, -- needs to be loaded in first
-  config = function()
-    require("tiny-inline-diagnostic").setup()
-    vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
-  end,
+  -- "rachartier/tiny-inline-diagnostic.nvim",
+  -- -- VeryLazy, Or LspAttach
+  -- event = "LspAttach",
+  -- priority = 1000, -- needs to be loaded in first
+  -- config = function()
+  --   require("tiny-inline-diagnostic").setup()
+  --   vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
+  -- end,
 }
