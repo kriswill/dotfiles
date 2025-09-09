3182efa38dcd46441b62f4a5ac3537dd631e5350
kriswill <115474+kriswill@users.noreply.github.com>
2025-09-09 14:52:57 -0700

    fix deprecation warning for home-manager programs.ssh default values

--- modules/home-manager/mixins/ssh.nix
+++ modules/home-manager/mixins/ssh.nix
@@ -20,8 +20,9 @@
       programs.ssh = {
         enable = true;
         includes = [ "${config.home.homeDirectory}/.ssh/config.d/*" ];
-        forwardAgent = true;
+        matchBlocks."*".forwardAgent = true;
         extraConfig = if isDarwin then darwin-extra-config else linux-extra-config;
+        enableDefaultConfig = false;
       };
       #sops.secrets.hosts = {
       #  sopsFile = ../../secrets/ssh.yaml;
