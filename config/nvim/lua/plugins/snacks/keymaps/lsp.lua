0dbe9c8e14a40d3c91880f4e3ad0f434aae4a3ae
kriswill <115474+kriswill@users.noreply.github.com>
2025-09-09 13:51:41 -0700

    neovim: move snacks picker for lsp references to <leader>sr

--- config/nvim/lua/plugins/snacks/keymaps/lsp.lua
+++ config/nvim/lua/plugins/snacks/keymaps/lsp.lua
@@ -2,7 +2,8 @@
 return {
   { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
   { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
-  { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+  { "<leader>sr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "LSP References Picker" },
   { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
   { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
 }
+
