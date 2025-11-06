cf301f54f17e6a886b287ced55a6ea22c7969c82
Kris Williams <115474+kriswill@users.noreply.github.com>
2025-11-06 10:39:16 -0800

    update flake - fix gitConfig deprecations

--- modules/home-manager/mixins/git.nix
+++ modules/home-manager/mixins/git.nix
@@ -10,34 +10,10 @@
 
   config = lib.mkIf config.kriswill.git.enable (
     let
-      gitConfig = {
-        core = {
-          editor = "nvim";
-          pager = "diff-so-fancy | less --tabs=4 -RFX";
-        };
-        init.defaultBranch = "main";
-        merge = {
-          conflictStyle = "diff3";
-          tool = "vim_mergetool";
-        };
-        mergetool."vim_mergetool" = {
-          cmd = ''nvim -f -c "MergetoolStart" "$MERGED" "$BASE" "$LOCAL" "$REMOTE"'';
-          prompt = false;
-        };
-        pull.rebase = false;
-        push.autoSetupRemote = true;
-        url = {
-          "https://github.com/".insteadOf = "gh:";
-          "ssh://git@github.com".pushInsteadOf = "gh:";
-          "https://gitlab.com/".insteadOf = "gl:";
-          "ssh://git@gitlab.com".pushInsteadOf = "gl:";
-        };
-      };
-
       rg = "${pkgs.ripgrep}/bin/rg";
     in
     {
-      home.packages = with pkgs.gitAndTools; [
+      home.packages = with pkgs; [
 
         diff-so-fancy # git diff with colors
         git-crypt # git files encryption
@@ -48,32 +24,56 @@
 
       programs.git = {
         enable = true;
-        aliases = {
-          amend = "commit --amend -m";
-          fixup = "!f(){ git reset --soft HEAD~\${1} && git commit --amend -C HEAD; };f";
-          # lines of code
-          loc = ''!f(){ git ls-files | ${rg} "\.''${1}" | xargs wc -l; };f'';
-          br = "branch";
-          co = "checkout";
-          st = "status";
-          ls = ''log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate'';
-          ll = ''log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --numstat'';
-          cm = "commit -m";
-          ca = "commit -am";
-          dc = "diff --cached";
-        };
-        extraConfig = gitConfig;
         ignores = [
           "*.direnv"
           "*.envrc"
           ".DS_Store"
         ];
-        #signing = {
-        #  key = "121D4302A64B2261";
-        #  signByDefault = true;
-        #};
-        userEmail = "115474+kriswill@users.noreply.github.com";
-        userName = "Kris Williams";
+        settings = {
+          alias = {
+            amend = "commit --amend -m";
+            fixup = "!f(){ git reset --soft HEAD~\${1} && git commit --amend -C HEAD; };f";
+            # lines of code
+            loc = ''!f(){ git ls-files | ${rg} "\.''${1}" | xargs wc -l; };f'';
+            br = "branch";
+            co = "checkout";
+            st = "status";
+            ls = ''log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate'';
+            ll = ''log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --numstat'';
+            cm = "commit -m";
+            ca = "commit -am";
+            dc = "diff --cached";
+          };
+          user = {
+            email = "115474+kriswill@users.noreply.github.com";
+            name = "Kris Williams";
+          };
+          core = {
+            editor = "nvim";
+            pager = "diff-so-fancy | less --tabs=4 -RFX";
+          };
+          init.defaultBranch = "main";
+          merge = {
+            conflictStyle = "diff3";
+            tool = "vim_mergetool";
+          };
+          mergetool."vim_mergetool" = {
+            cmd = ''nvim -f -c "MergetoolStart" "$MERGED" "$BASE" "$LOCAL" "$REMOTE"'';
+            prompt = false;
+          };
+          pull.rebase = false;
+          push.autoSetupRemote = true;
+          url = {
+            "https://github.com/".insteadOf = "gh:";
+            "ssh://git@github.com".pushInsteadOf = "gh:";
+            "https://gitlab.com/".insteadOf = "gl:";
+            "ssh://git@gitlab.com".pushInsteadOf = "gl:";
+          };
+          #signing = {
+          #  key = "121D4302A64B2261";
+          #  signByDefault = true;
+          #};
+        };
       }
       // (pkgs.sxm.git or { });
     }
