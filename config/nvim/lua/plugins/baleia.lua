d048dfa8f09e1867c2bd3d54e7d1c5562f6c5ebd
kriswill <115474+kriswill@users.noreply.github.com>
2025-12-20 15:00:50 -0800

    nvim: add baleia plugin - update lazy-lock

---
+++ config/nvim/lua/plugins/baleia.lua
@@ -0,0 +1,16 @@
+return {
+  "m00qek/baleia.nvim",
+  version = "*",
+  config = function()
+    local baleia = require("baleia").setup({})
+
+    -- Command to colorize current buffer on demand
+    vim.api.nvim_create_user_command("BaleiaColorize", function() baleia.once(vim.api.nvim_get_current_buf()) end, {})
+
+    -- Optional: auto-colorize specific buffers
+    vim.api.nvim_create_autocmd("BufWinEnter", {
+      pattern = { "*.log", "*conform*.log" },
+      callback = function() baleia.automatically(vim.api.nvim_get_current_buf()) end,
+    })
+  end,
+}
