9b1cae15d1f25827050fe6aa48ee3657b2e96c6d
Kris Williams <115474+kriswill@users.noreply.github.com>
2026-01-13 12:56:00 -0800

    update claude code 2.0.76 -> 2.0.77

--- pkgs/claude-code/package.nix
+++ pkgs/claude-code/package.nix
@@ -7,14 +7,14 @@
 }:
 buildNpmPackage (finalAttrs: {
   pname = "claude-code";
-  version = "2.0.76";
+  version = "2.0.77";
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${finalAttrs.version}.tgz";
-    hash = "sha256-46IqiGJZrZM4vVcanZj/vY4uxFH3/4LxNA+Qb6iIHDk=";
+    hash = "sha256-qC5hEaNDk1QfC7OJHJxuYXEhpys9vM9FZvBWOFBHzH0=";
   };
 
-  npmDepsHash = "sha256-mDErPWWqOe+3fKriTBLNCzXP48pmmlOMoB+kCP4FoT8=";
+  npmDepsHash = "sha256-4cCmdDQV+8P0uYCIW4Q72Wn7x8/nDCb757nBZxbs5Pk=";
 
   postPatch = ''
     cp ${./package-lock.json} package-lock.json
