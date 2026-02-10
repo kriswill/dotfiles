9fcc8ba8b7b997515515df41d6b763214940a354
kriswill <115474+kriswill@users.noreply.github.com>
2026-02-09 16:21:58 -0800

    add tmux-which-key plugin

--- modules/home-manager/tmux/default.nix
+++ modules/home-manager/tmux/default.nix
@@ -10,11 +10,18 @@
     let
       configDir = config.home.homeDirectory + "/src/dotfiles/config/tmux";
       ln = config.lib.file.mkOutOfStoreSymlink;
+      whichKey = pkgs.tmuxPlugins.tmux-which-key;
     in
     {
       home.packages = [ pkgs.tmux ];
       xdg.configFile = {
         "tmux/tmux.conf".source = ln configDir + "/tmux.conf";
+        "tmux/plugins.conf".text = ''
+          set -g @tmux-which-key-xdg-enable 1
+          run-shell 'mkdir -p ~/.local/share/tmux/plugins/tmux-which-key && touch ~/.local/share/tmux/plugins/tmux-which-key/init.tmux'
+          run-shell ${whichKey.rtp}
+        '';
+        "tmux/plugins/tmux-which-key/config.yaml".source = ln configDir + "/config.yaml";
       };
     }
   );
